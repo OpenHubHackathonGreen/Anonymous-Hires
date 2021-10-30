4259aef1cc1d5d9712b5a7be13f0f102afdab525
Dominick Piaquadio <dpiaquadio@gmail.com>
2021-10-30 10:25:03 -0400

    Fixed typos

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employee(request):
+	msg = "This is Employee dashboard"
+	context = {
+		'msg': msg
+	}
+	return render(request, "employee/employeedashboard.html", context)
