9baae06ffa5aec6d6c7c62aa6bce8a5e4877231d
Dominick Piaquadio <dpiaquadio@gmail.com>
2021-10-30 01:09:18 -0400

    Maybe 3rd time's the Charm?

--- main/views.py
+++ main/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	msg = "Something magnificent is being built here!"
+	context = {'msg': msg}
 	return render(request, 'index.html', context)
