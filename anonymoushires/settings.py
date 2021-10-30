65df746c50b5e6eacc84864cea9aff5dd270b82a
Dominick Piaquadio <dpiaquadio@gmail.com>
2021-10-30 10:47:13 -0400

    Configuring MYSQL database

--- anonymoushires/settings.py
+++ anonymoushires/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'anonymoushires.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'anonymoushires',
+        'USER': 'anonyhires',
+        'PASSWORD': 'anonymoushires',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
