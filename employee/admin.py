5f7b3b2a7a2bb05fda86af86135d911293ce4f9a
Dominick Piaquadio <dpiaquadio@gmail.com>
2021-10-30 13:19:21 -0400

    Register Entity with Admin

--- employee/admin.py
+++ employee/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from main.models import Entity
+from main.models import Entity, EntityType, State, Location
 # Register your models here.
 class EntityAdmin(admin.ModelAdmin):
 	fields = ['user_id','company_name', 'first_name', 'last_name', 'address', 'entity_type']
 
+class EntityTypeAdmin(admin.ModelAdmin):
+	fields = ['e_type']
+
+class StateAdmin(admin.ModelAdmin):
+	fields = ['name', 'abbr']
+
+class LocationAdmin(admin.ModelAdmin):
+	fields = ['street', 'city', 'state', 'zip_code']
+
 admin.site.register(Entity, EntityAdmin)
+admin.site.register(EntityType, EntityTypeAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(Location, LocationAdmin)
