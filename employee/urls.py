852328ebafad1c267eba9979c534a2544ced6014
Dominick Piaquadio <dpiaquadio@gmail.com>
2021-10-30 10:15:38 -0400

    Added to employee and employer app

--- employee/urls.py
+++ employee/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('employee', views.employer, name='employee'),
+	path('employee', views.employer, name='index'),
 ]
--- employer/urls.py
+++ employer/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('employer', views.employer, name='employer'),
+	path('employer', views.employer, name='index'),
 ]
