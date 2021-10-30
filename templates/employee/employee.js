b82cf137ba2a33b5f5a9bdc2823564b499f7e26a
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2021-10-30 13:10:00 -0400

    Changes in employee page
    - Added job company name to listing and be dispalyed

--- templates/employee/employee.js
+++ templates/employee/employee.js
@@ -2,16 +2,24 @@
 const skills = {"0":{ "name": "Python"}, "1":{ "name": "Web"}, "2":{ "name": "Java"}, "3":{ "name": "Evil buisness"}, "4":{ "name": "Cleaning"}, "5":{ "name": "Manical Laughing"}, "6":{ "name": "Cat Stroking"}, "7":{ "name": "Cooking"}, "8":{ "name": "Art"}};
 //Hard-coded list of listings a company has already made (will be pulled from server dynamically)
 let listings = {
-                    "0": {"name": "DevOps", "description": "Develop software in an agile enviornment", 
+                    "0": {
+                        "companyName": "PandaCorp",
+                        "name": "DevOps", "description": "Develop software in an agile enviornment", 
                         "skills": ["0", "2"], "hasApplied": false
                     }, 
-                    "1": {"name": "Evil Logistics", "description": "Concoct only the most nefarious of supply chains", 
+                    "1": {
+                        "companyName": "GeniusCrayons",
+                        "name": "Evil Logistics", "description": "Concoct only the most nefarious of supply chains", 
                         "skills": ["1"], "hasApplied": false
                     }, 
-                    "2": {"name": "Devious Chef", "description": "Cook who knows has mastered all 27 arts of poison and cookies", 
+                    "2": {
+                        "companyName": "SpilledMilk",
+                        "name": "Devious Chef", "description": "Cook who knows has mastered all 27 arts of poison and cookies", 
                         "skills": ["3"], "hasApplied": false
                     }, 
-                    "3": {"name": "Software Engineer", "description": "Work in a development enviornment", 
+                    "3": {
+                        "companyName": "PandaCorp",
+                        "name": "Software Engineer", "description": "Work in a development enviornment", 
                         "skills": ["1", "2"], "hasApplied": false
                     }
                 };
@@ -23,11 +31,12 @@ function initListings(){
     listings = queryData("listings");
     for(let key in listings)
         //Add listing to page as a tile
-        addTile(key, listings[key].name, 'listingsList', listingClick);
+        addTile(key, listings[key].name, 'listingsList', listingClick, listings[key].companyName);
 }
 //Load a listing onto the page to show current applicants
 function loadListing(listingId){
     //Set title and description
+    document.getElementById("listingCompanyName").innerHTML = listings[listingId].companyName;
     document.getElementById("listingTitle").innerHTML = listings[listingId].name;
     document.getElementById("listingDescription").innerHTML = listings[listingId].description;
     //Add skills tiles for listing
@@ -39,27 +48,18 @@ function loadSkills(skillList){
     document.getElementById("itemList").innerHTML = "";
     //Add tile for every skill in the list
     for(let i=0; i<skillList.length; i++)
-        addTile(skillList[i], skills[skillList[i]].name, "itemList", function(){});
-}
-//Adds a tile to the page based on the input of the given element id to the given parent list id element
-function addTileByInput(id, inputId, parentId, clearInput, onTileClick){
-    //Call add tile
-    addTile(id, document.getElementById(inputId).value, parentId, onTileClick);
-    //clear the input of the input element
-    if(clearInput){
-        document.getElementById(inputId).value = "";
-        //Refresh the search bar if that is the input
-        const event = new CustomEvent('input', { "detail": "" });
-        document.getElementById(inputId).dispatchEvent(event);
-    }
+        addTile(skillList[i], skills[skillList[i]].name, "itemList", function(){}, null);
 }
 //Adds a tile to the page based on the input text and the given parent list id element
-function addTile(id, name, parentId, onTileClick){
+function addTile(id, name, parentId, onTileClick, companyName){
     //Create a new list element
     let tile = document.createElement("LI");
     tile.className = "w3-blue";
     //Add text to the list element
-    tile.innerHTML = "<p>"+name+"</p>";
+    if(companyName != null){
+        tile.innerHTML = "<p>" + companyName + "</p>"
+    }
+    tile.innerHTML += "<p>"+name+"</p>";
     tile.id = "tile"+id;
     //Give onclick functionality
     tile.onclick = onTileClick;
